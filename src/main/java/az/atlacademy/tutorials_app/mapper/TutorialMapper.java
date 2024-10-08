f9956fe1b00c6b9d4ac57c2e7f0cf08c020ec8ef
Javid Sadigli <cavidoffical@gmail.com>
2024-10-08 15:41:29 +0400

    Added mapstruct

--- src/main/java/az/atlacademy/tutorials_app/mapper/TutorialMapper.java
+++ src/main/java/az/atlacademy/tutorials_app/mapper/TutorialMapper.java
@@ -1,51 +1,17 @@
 package az.atlacademy.tutorials_app.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import az.atlacademy.tutorials_app.model.dto.TutorialRequestDTO;
 import az.atlacademy.tutorials_app.model.dto.TutorialResponseDTO;
 import az.atlacademy.tutorials_app.model.entity.TutorialEntity;
 
-@Component
-public class TutorialMapper 
+@Mapper(componentModel = "spring")
+public interface TutorialMapper 
 {
-    public TutorialResponseDTO entityToResponseDTO(TutorialEntity entity)
-    {
-        try
-        {
-            return TutorialResponseDTO.builder()
-                    .id(entity.getId())
-                    .title(entity.getTitle())
-                    .description(entity.getDescription())
-                    .published(entity.isPublished())
-                    .build();
-        }
-        catch (NullPointerException e)
-        {
-            return null;
-        }
-    }
+    public TutorialEntity requestDTOToEntity(TutorialRequestDTO dto);
+    public TutorialResponseDTO entityToResponseDTO(TutorialEntity entity);
 
-    public TutorialEntity requestDTOToEntity(TutorialRequestDTO dto)
-    {
-        try
-        {
-            return TutorialEntity.builder()
-                    .title(dto.getTitle())
-                    .description(dto.getDescription())
-                    .published(dto.isPublished())
-                    .build();
-        }
-        catch (NullPointerException e)
-        {
-            return null;
-        }
-    }
-
-    public void convertRequestToEntity(TutorialRequestDTO dto, TutorialEntity entity)
-    {
-        entity.setTitle(dto.getTitle());
-        entity.setDescription(dto.getDescription());
-        entity.setPublished(dto.isPublished());
-    }
+    public void convertRequestToEntity(TutorialRequestDTO requestDTO, @MappingTarget TutorialEntity entity); 
 }
